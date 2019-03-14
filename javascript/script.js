5f5473b232754212c27d70e9221b03a735290809
BeAirez <martijn7000@gmail.com>
2019-03-14 14:51:32 +0100

    Optimised Code

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,45 @@
 // De actuele tijd
-function startTime() {
-	var today = new Date();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	m = checkTime(m);
-	s = checkTime(s);
+function startTijd() {
+	var vandaag = new Date();
+	var uur 	= vandaag.getHours();
+	var minuut 	= vandaag.getMinutes();
+	var seconde = vandaag.getSeconds();
+	minuut 		= checkTijd(minuut);
+	seconde 	= checkTijd(seconde);
 	document.getElementById('txt').innerHTML =
-	h + ":" + m + ":" + s;
-	var t = setTimeout(startTime, 500);
+	uur + ":" + minuut + ":" + seconde;
+	var t = setTimeout(startTijd, 1000);
 }
 //Deze functie voegt een 0 toe aan de klok wanneer het nummer lager dan 10 is.
-function checkTime(i) {
+function checkTijd(i) {
 	if (i < 10) {i = "0" + i};
 	return i;
 }
 
 //De dag van de week
-function dag() {
-	var d = new Date();
-	var weekday = ['Sunday...', 'Monday...', 'Tuesday..', 'Wednesday', 'Thursday.', 'Friday...', 'Saturday.'];
-	var n = weekday[d.getDay()];
-	document.getElementById("day").innerHTML = n;
+function weekDag() {
+	var dag = new Date();
+	var dagNaam = ['Sunday...', 'Monday...', 'Tuesday..', 'Wednesday', 'Thursday.', 'Friday...', 'Saturday.'];
+	var dagVanDeWeek = dagNaam[dag.getDay()];
+	document.getElementById("day").innerHTML = dagVanDeWeek;
 }
 
 //Veranderd de achtergrond kleur op basis van tijd.
 var now = new Date();
-var hours = now.getHours();
+var tijd = now.getHours();
 
-if (hours > 8 && hours < 18){
-	document.write ('<body style="background-color: rgba(245,245,240 ,1 ); color: black;">');
+if (tijd > 8 && tijd < 18){
+	document.body.style.backgroundColor = "rgba(245, 245, 240 ,1 )";
+	document.body.style.color = "black";
 }
 else {
-	document.write ('<body style="background-color: black; color: white;">');
+	document.body.style.backgroundColor = "rgba(0, 0, 0 ,1 )";
+	document.body.style.color = "white";
 }
 
 //Tweenmax animaties
+TweenMax.from('#txt', 2, {opacity:0});
+TweenMax.from('#logo', 0.5, {scale:.5, opacity: 0, delay:3.2, ease:Back.easeOut.config(2)});
 
-
+startTijd();
+weekDag();
